1354053484b81235e6414ebe159613403221124d
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2021-03-16 11:57:44 -0500

    Se borro codigo muerto

--- src/models/discipline.model.js
+++ src/models/discipline.model.js
@@ -4,17 +4,6 @@ const disciplineSchema = new Schema({
   name: {
     type: String,
     required: true,
-    // validate: {
-    //   async validator(value) {
-    //     try {
-    //       const discipline = await models.Disciplines.findOne({ value })
-    //       return !discipline
-    //     } catch(err) {
-    //       return false
-    //     }
-    //   },
-    //   message: 'La disciplina ya existe.',
-    // },
   }
 }, {
     timestamps: true,
