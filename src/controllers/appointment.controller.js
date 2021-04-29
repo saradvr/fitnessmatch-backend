9e9b71366f213babf9a416a8671137496274df45
saradvr <saradvr1608@gmail.com>
2021-04-28 20:39:16 -0500

    creacion entidad transacciones y modificacion de citas para el pago

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -13,9 +13,38 @@ module.exports = {
       const coach = await Coach.findByIdAndUpdate(body.coachId, {$push: { appointments: appointment._id }}, {new: true})
       coach.availableHours = coach.availableHours.filter(item => item !== appointment.appointmentDate)
       await coach.save({ validateBeforeSave: false })
-      res.status(201).json({ message: "Cita creada con éxito", appointment })
+      res.status(201).json({ message: "Cita reservada con éxito", appointment })
     } catch(error) {
       res.status(400).json({ message: "Su cita no pudo crearse, intente nuevamente.", error })
     }
   },
+  async getAppointment(req, res) {
+    try {
+      const { params: { appointmentId } } = req
+      const appointment = await Appointment.findById(appointmentId)
+      res.status(201).json({message: "Cita cargada con éxito", appointment})
+    } catch (error) {
+      res.status(400).json({message: "La cita no pudo cargarse", error})
+    }
+  },
+  async updateAppointment(req,res) {
+    try {
+      const { body: {appointmentId, status} } = req
+      const appointment = await Appointment.findByIdAndUpdate(appointmentId, status, {new: true})
+      res.status(201).json({ message: "Cita actualizada con éxito", appointment })
+    } catch (error) {
+      res.status(400).json({ message: "Su cita no pudo actualizarse, intente nuevamente.", error })
+    }
+  },
+  async deleteAppointment(req,res) {
+    try {
+      const { body: { appointmentId, coachId }, user: { userTypeId } } = req
+      const appointment = await Appointment.findByIdAndDelete(appointmentId)
+      const coach = await Coach.findByIdAndUpdate(coachId, {$push: { availableHours: appointment.appointmentDate }, $pull: { appointments: appointment._id }}, {new: true})
+      const client = await Client.findByIdAndUpdate(userTypeId, {$pull: { appointments: appointment._id }}, {new: true})
+      res.status(201).json({ message: "Cita eliminada con éxito", appointment, coach, client })
+    } catch (error) {
+      res.status(400).json({ message: "Su cita no pudo eliminarse, intente nuevamente.", error })
+    }
+  }
 }
